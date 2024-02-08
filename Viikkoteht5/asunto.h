9102dda1d453d7736387eb75a59378e520e7ff7f
unknown <tuisi.tavi@gmail.com>
2024-02-08 12:45:52 +0200

    viikkoteht5

---
+++ Viikkoteht5/asunto.h
@@ -0,0 +1,16 @@
+#ifndef ASUNTO_H
+#define ASUNTO_H
+
+class Asunto
+{
+public:
+    int asukasMaara;
+    int neliot;
+    Asunto();
+    void maarita(int,int);
+    double laskekulutus(double);
+
+
+};
+
+#endif // ASUNTO_H
